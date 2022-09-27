a334426550270e3921bd7b0d21922a29c166297c
Markus Hädrich <markus.haedrich@mail.de>
2022-09-27 22:43:02 +0200

    Func for getting lan device IP added, but not in use now

--- RTKHTDemo/MainViewController.swift
+++ RTKHTDemo/MainViewController.swift
@@ -57,12 +57,46 @@ class MainViewController: UIViewController {
   let headTrackerCharacteristicCBUUID = CBUUID(string: "713D0002-503E-4C75-BA94-3148F18D941E")
   let realTimeKinematicsCharacteristicCBUUID = CBUUID(string: "713D0004-503E-4C75-BA94-3148F18D941E")
   let rtkAccuracyCharacteristicCBUUID = CBUUID(string: "713D0006-503E-4C75-BA94-3148F18D941E")
-  let deviceNamePrefix = "RTKRover_"
+  let deviceNamePrefix = "rtkrover"
   var centralManager: CBCentralManager!
   var headTrackerPeripheral: CBPeripheral!
   var headtrackerDeviceName = ""
   var rtkPositionAnnotation = MKPointAnnotation()
   let regionRadius: CLLocationDistance = 25
+  
+/**
+        For later: Function to get the IP address of devices in  lan
+ */
+//  func getIPAddress() -> String {
+//      var address: String?
+//      var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+//      if getifaddrs(&ifaddr) == 0 {
+//          var ptr = ifaddr
+//          while ptr != nil {
+//              defer { ptr = ptr?.pointee.ifa_next }
+//
+//              guard let interface = ptr?.pointee else { return "" }
+//              let addrFamily = interface.ifa_addr.pointee.sa_family
+//              if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+//
+//                  // wifi = ["en0"]
+//                  // wired = ["en2", "en3", "en4"]
+//                  // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
+//
+//                  let name: String = String(cString: (interface.ifa_name))
+//                  if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+//                      var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+//                      getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+//                      address = String(cString: hostname)
+//                    print("hostname", hostname)
+//                  }
+//              }
+//          }
+//          freeifaddrs(ifaddr)
+//      }
+//      return address ?? ""
+//  }
+
   
   func setupUI() {
     navigationController?.navigationBar.barTintColor = UIColor.green
@@ -119,6 +153,8 @@ class MainViewController: UIViewController {
     deviceNameTextField.backgroundColor = UIColor.white
     deviceNameTextField.textColor = UIColor.blue
     deviceNameTextField.borderStyle = .none
+    
+//    print("wifi: \(String(describing: getIPAddress()))")
 
   }
  
